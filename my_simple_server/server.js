a98815d118a5b209ea673894162b4f87edc32ade
Janet <janetowen07@gmail.com>
2017-06-10 18:34:16 +0100

    added a writeable stream that writes data to another file

--- my_simple_server/server.js
+++ my_simple_server/server.js
@@ -1,17 +1,21 @@
 const http = require('http');
 const fs = require('fs');
 
-let myReadStream = fs.createReadStream(__dirname + '/users.txt', 'utf8');
-
-myReadStream.on('data', function(chunk) {
-    console.log('new chunk received');
-    console.log(chunk);
-});
-
 let server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello everyone');
 });
+
+let myReadStream = fs.createReadStream(__dirname + '/users.txt', 'utf8');
+let myWriteStream = fs.createWriteStream(__dirname + '/users2.txt');
+
+myReadStream.on('data', function(chunk) {
+    console.log('new chunk received');
+    console.log(chunk);
+    myWriteStream.write(chunk);
+});
+
+
 
 server.listen(3010, '127.0.0.1');
 console.log('Listening to port 3010');
