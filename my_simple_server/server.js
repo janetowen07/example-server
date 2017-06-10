8b5694a7ec7d0985747368933e850aeeeda0580d
Janet <janetowen07@gmail.com>
2017-06-10 18:25:55 +0100

    added a readable stream for users.txt

--- my_simple_server/server.js
+++ my_simple_server/server.js
@@ -1,6 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
-var server = http.createServer(function(req, res) {
+let myReadStream = fs.createReadStream(__dirname + '/users.txt', 'utf8');
+
+myReadStream.on('data', function(chunk) {
+    console.log('new chunk received');
+    console.log(chunk);
+});
+
+let server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello everyone');
 });
