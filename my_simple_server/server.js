3aadfcc83c9e44a98df36dcb9c19baf1164ecc5c
Janet <janetowen07@gmail.com>
2017-06-10 19:13:54 +0100

    replaced read and write with a pipe

--- my_simple_server/server.js
+++ my_simple_server/server.js
@@ -1,21 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
-let server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello everyone');
+let server = http.createServer(function (req, res) {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    let myReadStream = fs.createReadStream(__dirname + '/users.txt', 'utf8');
+    myReadStream.pipe(res);
+    // res.end('Hello everyone');
 });
-
-let myReadStream = fs.createReadStream(__dirname + '/users.txt', 'utf8');
-let myWriteStream = fs.createWriteStream(__dirname + '/users2.txt');
-
-myReadStream.on('data', function(chunk) {
-    console.log('new chunk received');
-    console.log(chunk);
-    myWriteStream.write(chunk);
-});
-
-
 
 server.listen(3010, '127.0.0.1');
 console.log('Listening to port 3010');
